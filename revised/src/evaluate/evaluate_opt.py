a838a6d7fca6982ce9283f67d7e394f85e95d470
Arjun Sripathy <arjun.sripathy2000@gmail.com>
2018-11-26 16:58:07 -0800

    Added average word counts to evaluation script

--- revised/src/evaluate/evaluate_opt.py
+++ revised/src/evaluate/evaluate_opt.py
@@ -56,50 +56,58 @@ rouge = Rouge()
 num_thresholds = 102
 thresholds = [i/100 for i in range(num_thresholds)]
 
-#Stores rouge scores for each threshold on each document
-val_rouge = [{'rouge-1':0,'rouge-2':0,'rouge-l':0} for _ in range(num_thresholds)]
-
-#data and predictions for validation dataset stored in one data structure
-val_data = [{'text_labels':labels[d_id],
-	'abstract': abstracts[d_id],
-	'full_text_sentences': full_text_sentences[d_id],
-	'probabilities': predictions[d_id]}
-	for d_id in val_docs if abstracts[d_id]][:NUM_VAL_DOCS]
-
-
-count = 0
-
-print(f"Evaluating Thresholds on {NUM_VAL_DOCS} Validation documents...")
-for d in val_data:
-	count+=1
-	print(f"Analyzing {count}/{NUM_VAL_DOCS}",end='\r')
-	p = d['probabilities']
-	#Prediction may only consider early sentences
-	#Have it predict 0 for all other sentences
-	p+=[0]*(len(d['text_labels'])-len(p))
-
-	for i,thresh in enumerate(thresholds):
-		summary = generateSummary(p,d['full_text_sentences'],thresh)
-		#summary will be "" if prediction entailed 0 length summary
-		if(summary):
-			rouge_scores = rouge.get_scores(summary,d['abstract'])[0]
-			#Increment scores
-			for r in ['rouge-1','rouge-2','rouge-l']:
-				val_rouge[i][r]+=rouge_scores[r]['f']
-print()
-print()
-print("Finding Optimal Threshold based on Validation Evaluation...")
 best_threshold = None
-#Maximizes combined rouge scores
-max_f = max(val_rouge,key=lambda m:(m['rouge-1']+m['rouge-2']+m['rouge-l']))
-for i,m in enumerate(val_rouge):
-	if(m==max_f):
-		best_threshold = i/100
-		break
-print("Optimal Model Threshold:",i/100)
-print()
+try:
+	best_threshold=float(sys.argv[3])
+	print(f"Threshold {best_threshold} provided")
+except:
+	print("No threshold provided.  Proceeding with Threshold Optimization...")
 
-best_threshold = 0.5
+#If threshold not provided 
+if(not best_threshold):
+	#Stores rouge scores for each threshold on each document
+	val_rouge = [{'rouge-1':0,'rouge-2':0,'rouge-l':0} for _ in range(num_thresholds)]
+
+	#data and predictions for validation dataset stored in one data structure
+	val_data = [{'text_labels':labels[d_id],
+		'abstract': abstracts[d_id],
+		'full_text_sentences': full_text_sentences[d_id],
+		'probabilities': predictions[d_id]}
+		for d_id in val_docs if abstracts[d_id]][:NUM_VAL_DOCS]
+
+
+	count = 0
+
+	print(f"Evaluating Thresholds on {NUM_VAL_DOCS} Validation documents...")
+	for d in val_data:
+		count+=1
+		print(f"Analyzing {count}/{NUM_VAL_DOCS}",end='\r')
+		p = d['probabilities']
+		#Prediction may only consider early sentences
+		#Have it predict 0 for all other sentences
+		p+=[0]*(len(d['text_labels'])-len(p))
+
+		for i,thresh in enumerate(thresholds):
+			summary = generateSummary(p,d['full_text_sentences'],thresh)
+			#summary will be "" if prediction entailed 0 length summary
+			if(summary):
+				rouge_scores = rouge.get_scores(summary,d['abstract'])[0]
+				#Increment scores
+				for r in ['rouge-1','rouge-2','rouge-l']:
+					val_rouge[i][r]+=rouge_scores[r]['f']
+	print()
+	print()
+	print("Finding Optimal Threshold based on Validation Evaluation...")
+	best_threshold = None
+	#Maximizes combined rouge scores
+	max_f = max(val_rouge,key=lambda m:(m['rouge-1']+m['rouge-2']+m['rouge-l']))
+	for i,m in enumerate(val_rouge):
+		if(m==max_f):
+			best_threshold = i/100
+			break
+	print("Optimal Model Threshold:",best_threshold)
+	print()
+
 test_docs = data_splits['test']
 if(not NUM_TEST_DOCS): NUM_TEST_DOCS = len(test_docs)
 
@@ -141,6 +149,12 @@ label_rouge = {'rouge-1':{'p':0,'r':0,'f':0},'rouge-2':{'p':0,'r':0,'f':0},'roug
 
 test_ROC = [{'tpr':0,'fpr':0} for _ in range(num_thresholds)]
 
+
+#cumulative word counts of sentences in summaries
+model_word_count = 0
+label_word_count = 0
+human_word_count = 0
+
 count=0
 
 print(f"Evalauating performance on {NUM_TEST_DOCS} test documents...")
@@ -169,6 +183,11 @@ for d in test_data:
 		for r in ['rouge-1','rouge-2','rouge-l']:
 			for m in ['p','r','f']:
 				label_rouge[r][m]+=rouge_scores[r][m]
+	#increment word count metrics
+	#word counts are the number of spaces in string + 1
+	model_word_count+=summary.count(' ')+1
+	label_word_count+=label_summary.count(' ')+1
+	human_word_count+=d['abstract'].count(' ')+1
 	#Get ROC Metrics
 	for i,thresh in enumerate(thresholds):
 		#Get threshold predicitions 0 if less than thresh 1 if greater
@@ -191,6 +210,10 @@ for t in test_ROC:
 	t['tpr']/=NUM_TEST_DOCS
 	t['fpr']/=NUM_TEST_DOCS
 
+model_word_count/=NUM_TEST_DOCS
+label_word_count/=NUM_TEST_DOCS
+human_word_count/=NUM_TEST_DOCS
+
 print()
 print("ROUGE Comparison (Model Performance vs Label Performance)")
 #Function used to get in percent format 0.123456 -> 12.35
@@ -206,6 +229,11 @@ for r in ['rouge-1','rouge-2','rouge-l']:
 	print(f"Recall:    {p(ms['r'])} vs {p(ls['r'])}")
 	print(f"F1 score:  {p(ms['f'])} vs {p(ls['f'])}")
 
+print()
+print("Average Word Counts")
+print(f"Model: {model_word_count} words")
+print(f"Label: {label_word_count} words")
+print(f"Human: {human_word_count} words")
 print()
 print("Creating ROC Curve")
 ROC_Curve_x = [m['fpr'] for m in test_ROC]

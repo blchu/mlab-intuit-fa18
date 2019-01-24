d29ba3bd5177dd85e2cdcdb0dc25701477d53c4c
Brenton Chu <brentonlongchu@berkeley.edu>
2019-01-24 15:32:57 -0800

    final preprocessing code

--- revised/src/preprocess/run_preprocess.py
+++ revised/src/preprocess/run_preprocess.py
@@ -8,7 +8,7 @@ from util.json_conversion import write_dict_to_json
 from util.spacy_tokenizer import tokenize_document, sentence_tokenize
 from labeling import get_binary_labels
 from data_split import split_data
-# from coref import resolve
+from coref import resolve
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -20,6 +20,8 @@ def parse_arguments():
                         help="Directory of output files")
     parser.add_argument('--replace', action='store_true',
                         help="Flag for optionally replacing pronouns for label generation")
+    parser.add_argument('--min_sentences', type=int, default=5,
+                        help="Minimum number of sentences in a document")
     args = parser.parse_args()
     return args
 
@@ -34,10 +36,16 @@ if __name__ == '__main__':
 
     for year in os.listdir(args.root_path):
         year_path = os.path.join(args.root_path, year)
-        for month in os.listdir(year_path)[:1]:
+        if not os.path.isdir(year_path):
+            continue
+        for month in os.listdir(year_path):
             month_path = os.path.join(year_path, month)
-            for day in os.listdir(month_path)[:1]:
+            if not os.path.isdir(month_path):
+                continue
+            for day in os.listdir(month_path):
                 day_path = os.path.join(month_path, day)
+                if not os.path.isdir(day_path):
+                    continue
                 for filename in os.listdir(day_path):
                     whole_path = os.path.join(day_path, filename)
                     abstract_string, fulltext_string = extract_text_from_xml(whole_path)
@@ -45,15 +53,19 @@ if __name__ == '__main__':
                         tokenized = tokenize_document(fulltext_string)
                         key = (args.source + '_' + year + '_' + month + '_' + day + '_'
                                + filename.replace('.xml', ''))
-                        abstracts[key] = abstract_string.strip()
-                        fulltexts[key] = tokenized
                         if args.replace:
                             replace_text = resolve(fulltext_string, tokenize)
                             sent_tokens = sentence_tokenize(replace_text)
                         else:
                             sent_tokens = sentence_tokenize(fulltext_string)
-                        labels[key] = get_binary_labels(abstract_string, sent_tokens)
-                        sentence_tokens[key] = sent_tokens
+                        if len(sent_tokens) > args.min_sentences:
+                            try:
+                                labels[key] = get_binary_labels(abstract_string, sent_tokens)
+                                abstracts[key] = abstract_string.strip()
+                                fulltexts[key] = tokenized
+                                sentence_tokens[key] = sent_tokens
+                            except ValueError:
+                                continue
                 print("Documents in %s/%s/%s processed" % (month, day, year))
 
     write_dict_to_json(abstracts, os.path.join(args.output_dir, 'abstracts.json'))
@@ -62,5 +74,5 @@ if __name__ == '__main__':
     write_dict_to_json(labels, os.path.join(args.output_dir, 'labels.json'))
     
     key_set = set(fulltexts.keys())
-    splits = split_data(key_set)
+    splits = split_data(key_set, num_train_subset=5000)
     write_dict_to_json(splits, os.path.join(args.output_dir, 'data_splits.json'))

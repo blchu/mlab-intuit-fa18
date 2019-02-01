5b688371ea5dab8084dd9576177f792917c69a7e
Brenton Chu <brentonlongchu@berkeley.edu>
2019-01-31 20:34:36 -0800

    preprocessing cnn

--- revised/src/preprocess/run_preprocess.py
+++ revised/src/preprocess/run_preprocess.py
@@ -1,9 +1,11 @@
 import argparse
 import os
 import sys
+import time
 
 sys.path.append('..')
 from util.read_xml import extract_text_from_xml
+from util.read_story import extract_text_from_story
 from util.json_conversion import write_dict_to_json
 from util.spacy_tokenizer import tokenize_document, sentence_tokenize
 from labeling import get_binary_labels
@@ -34,39 +36,59 @@ if __name__ == '__main__':
     sentence_tokens = {}
     labels = {}
 
-    for year in os.listdir(args.root_path):
-        year_path = os.path.join(args.root_path, year)
-        if not os.path.isdir(year_path):
-            continue
-        for month in os.listdir(year_path):
-            month_path = os.path.join(year_path, month)
-            if not os.path.isdir(month_path):
+    docs = []
+    if args.source == "nyt":
+        for year in os.listdir(args.root_path):
+            year_path = os.path.join(args.root_path, year)
+            if not os.path.isdir(year_path):
                 continue
-            for day in os.listdir(month_path):
-                day_path = os.path.join(month_path, day)
-                if not os.path.isdir(day_path):
+            for month in os.listdir(year_path):
+                month_path = os.path.join(year_path, month)
+                if not os.path.isdir(month_path):
                     continue
-                for filename in os.listdir(day_path):
-                    whole_path = os.path.join(day_path, filename)
-                    abstract_string, fulltext_string = extract_text_from_xml(whole_path)
-                    if abstract_string is not None and fulltext_string is not None:
-                        tokenized = tokenize_document(fulltext_string)
+                for day in os.listdir(month_path):
+                    day_path = os.path.join(month_path, day)
+                    if not os.path.isdir(day_path):
+                        continue
+                    for filename in os.listdir(day_path):
+                        whole_path = os.path.join(day_path, filename)
                         key = (args.source + '_' + year + '_' + month + '_' + day + '_'
                                + filename.replace('.xml', ''))
-                        if args.replace:
-                            replace_text = resolve(fulltext_string, tokenize)
-                            sent_tokens = sentence_tokenize(replace_text)
-                        else:
-                            sent_tokens = sentence_tokenize(fulltext_string)
-                        if len(sent_tokens) > args.min_sentences:
-                            try:
-                                labels[key] = get_binary_labels(abstract_string, sent_tokens)
-                                abstracts[key] = abstract_string.strip()
-                                fulltexts[key] = tokenized
-                                sentence_tokens[key] = sent_tokens
-                            except ValueError:
-                                continue
-                print("Documents in %s/%s/%s processed" % (month, day, year))
+                        docs.append((whole_path, key))
+    elif args.source == "cnn":
+        stories_path = os.path.join(args.root_path, "stories")
+        assert os.path.isdir(stories_path)
+        for filename in os.listdir(stories_path):
+            whole_path = os.path.join(stories_path, filename)
+            key = args.source + '_' + filename.replace('.story', '')
+            docs.append((whole_path, key))
+    else:
+        raise RuntimeError('Source is not recognized.')
+
+    t = time.time()
+    for i, (doc, key) in enumerate(docs):
+        if args.source == "nyt":
+            abstract_string, fulltext_string = extract_text_from_xml(doc)
+        if args.source == "cnn":
+            abstract_string, fulltext_string = extract_text_from_story(doc)
+        if abstract_string is not None and fulltext_string is not None:
+            tokenized = tokenize_document(fulltext_string)
+            if args.replace:
+                replace_text = resolve(fulltext_string, tokenize)
+                sent_tokens = sentence_tokenize(replace_text)
+            else:
+                sent_tokens = sentence_tokenize(fulltext_string)
+            if len(sent_tokens) > args.min_sentences:
+                try:
+                    labels[key] = get_binary_labels(abstract_string, sent_tokens)
+                    abstracts[key] = abstract_string.strip()
+                    fulltexts[key] = tokenized
+                    sentence_tokens[key] = sent_tokens
+                except ValueError:
+                    continue
+        est_time = ((time.time() - t)/(i+1))*(len(docs)-(i+1))
+        print("Document {} of {} completed. Estimated time remaining: {} seconds\r"
+              .format(i+1, len(docs), round(est_time,3)))
 
     write_dict_to_json(abstracts, os.path.join(args.output_dir, 'abstracts.json'))
     write_dict_to_json(fulltexts, os.path.join(args.output_dir, 'fulltexts.json'))
